b1422b3d86dc98d728a7fd9c392de1ff199a021b
eduardobio6 <101075205+eduardobio6@users.noreply.github.com>
2022-09-02 20:31:58 -0300

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,14 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   name = 'EduardoApp';
-  nome = 'Carlos Eduardo'
+  nome = 'Carlos Eduardo';
   nasc = 1997;
-  
-  getIdade(){
+  counter = 0;
+  timer = setInterval(() => {
+    this.counter = this.counter + 1;
+  }, 1000);
+
+  getIdade() {
     return 2022 - this.nasc;
   }
 }
