b94205f397bb9d975d7ddcffa2987982d75cb6a3
eduardobio6 <101075205+eduardobio6@users.noreply.github.com>
2022-09-02 20:22:33 -0300

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'EduardoApp';
+  nasc = 1997;
+  idade = 2022 - this.nasc;
 }
