0538c4b74371f72fc6d2f710dd38eab090588b55
eduardobio6 <101075205+eduardobio6@users.noreply.github.com>
2022-09-02 20:36:50 -0300

    7 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,17 @@ export class AppComponent {
   counter = 0;
   timer = setInterval(() => {
     this.counter = this.counter + 1;
-  }, 1000);
+  }, 100);
 
   getIdade() {
     return 2022 - this.nasc;
+  }
+
+  getMinutos() {
+    return Math.trunc(this.counter / 60);
+  }
+
+  getSegundos() {
+    return Math.trunc(this.counter % 60);
   }
 }
